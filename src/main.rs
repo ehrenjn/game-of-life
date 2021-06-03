378d61ecb3d14027e9ae2415a2fd66a9a2f9d5b3
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2021-06-03 16:18:29 -0400

    added full controls

--- src/main.rs
+++ src/main.rs
@@ -30,35 +30,50 @@ use std::io::{
 
 
 const INSTRUCTIONS: &str = "\
-    ║ spacebar: play/pause      ║\r\n\
-    ║ f:        forward 1 frame ║\r\n\
-    ║ r:        randomize       ║\r\n\
-    ║ q:        quit            ║\r\n\
-    ╚═══════════════════════════╝\
+    ║ spacebar:   play/pause       ║\r\n\
+    ║ arrow keys: move cursor      ║\r\n\
+    ║ c:          clear            ║\r\n\
+    ║ a:          create/kill cell ║\r\n\
+    ║ f:          advance 1 frame  ║\r\n\
+    ║ r:          randomize        ║\r\n\
+    ║ h:          show/hide cursor ║\r\n\
+    ║ q:          quit             ║\r\n\
+    ╚══════════════════════════════╝\
 ";
-const INSTRUCTIONS_WIDTH: u16 = 29;
+const INSTRUCTIONS_WIDTH: u16 = 32;
 
 
 // derive() will automatically derive all the traits needed to be hashable by autogenering an impl
 // PartialEq adds equal and not equal methods (for symmetric and transitive relationships)
 // Eq adds no methods but basically says "the reflexive property holds for this thing"
 // you cant just do derive(Eq) because Eq inherits PartialEq so you need those methods for Eq to hold
-#[derive(PartialEq, Eq, Hash)]
+// also derive Clone because I want to be able to clone Points
+#[derive(PartialEq, Eq, Hash, Clone)]
 struct Point {
-    x: usize, // needs to be usize because I can't index into a vector with just any old integer
-    y: usize,
+    x: i16,
+    y: i16,
+}
+
+
+impl Point {
+    fn bound(&mut self, min_x: i16, min_y: i16, max_x: i16, max_y: i16) {
+        if self.x < min_x { self.x = min_x; } 
+        else if self.x > max_x { self.x = max_x; }
+        if self.y < min_y { self.y = min_y; } 
+        else if self.y > max_y { self.y = max_y; }
+    }
 }
 
 
 struct Board {
-    width: usize,
-    height: usize,
+    width: u32,
+    height: u32,
     occupied_cells: HashSet<Point>,
 }
 
 
 impl Board {
-    fn new(width: usize, height: usize) -> Board {
+    fn new(width: u32, height: u32) -> Board {
         return Board {
             width: width,
             height: height,
@@ -71,8 +86,8 @@ impl Board {
         let mut rng = rand::thread_rng();
         for _ in 0..((self.width * self.height) / 4) {
             let random_cell = Point{
-                x: rng.gen_range(0..self.width),
-                y: rng.gen_range(0..self.height)
+                x: rng.gen_range(0..self.width as i16),
+                y: rng.gen_range(0..self.height as i16)
             };
             self.occupied_cells.insert(random_cell);
         }
@@ -85,8 +100,8 @@ impl Board {
         for cell in &self.occupied_cells {
 
             let on_top_edge = cell.y == 0;
-            let on_right_edge = cell.x == (self.width - 1);
-            let on_bottom_edge = cell.y == (self.height - 1);
+            let on_right_edge = cell.x == (self.width as i16 - 1);
+            let on_bottom_edge = cell.y == (self.height as i16 - 1);
             let on_left_edge = cell.x == 0;
 
             // find all valid neighbours
@@ -148,7 +163,7 @@ impl fmt::Display for Board {
         let mut board_string = Vec::new();
         for _ in 0..self.height {
             let mut cell_row: Vec<char> = vec!['║'];
-            cell_row.extend(iter::repeat(' ').take(self.width));
+            cell_row.extend(iter::repeat(' ').take(self.width as usize));
             cell_row.push('║');
             cell_row.push('\r'); // in raw mode terminals a newline just moves the cursor down, we need a carriage return so that the cursor also moves to the beginning of the line
             cell_row.push('\n');
@@ -157,7 +172,7 @@ impl fmt::Display for Board {
 
         // add filled cells
         for point in &self.occupied_cells {
-            board_string[point.y][point.x+1] = '⬤';//'◯';//'◉';//'▨'; // x+1 because the first character of every row is a '║'
+            board_string[point.y as usize][point.x as usize + 1] = '⬤';//'◯';//'◉';//'▨'; // x+1 because the first character of every row is a '║'
         }
 
         // convert vector to string and print
@@ -174,7 +189,9 @@ fn print_static_text<W: Write>(stdout: &mut W, board: &Board) {
     // print top and bottom of board
     write!(stdout, "{}", termion::clear::All); // .ok() to convert Result into an Option and throw away the possible Error (because not handling the error is a compiler warning)
     write!(stdout, "{}╔", termion::cursor::Goto(1, 1));
-    let long_pipe: String = iter::repeat('═').take(board.width).collect();
+    let long_pipe: String = iter::repeat('═')
+        .take(board.width as usize)
+        .collect();
     write!(stdout, "{}", long_pipe);
     write!(stdout, "╗");
     write!(
@@ -198,11 +215,21 @@ fn print_static_text<W: Write>(stdout: &mut W, board: &Board) {
 fn play_game<W: io::Write, R: io::Read>(board: &mut Board, key_input: &mut termion::input::Keys<R>, stdout: &mut W) {
     let mut paused = false;
     let mut game_running = true;
+    let mut cursor_position = Point{x:0, y:0}; // we will consider the top left of the board to be 0,0 to conform with board.occupied_cells Points
+    let mut cursor_visible = true;
 
     while game_running {
 
-        let mut board_updated = false; // if set to true then we should draw the board even if the game is currently paused
+        let mut board_updated = false;
 
+        // update_cells before we handle key presses so that if a keypress causes a cell to be born or die we will see that effect directly on the next frame (if we were to call update_cells after handling input (but before printing the frame) then we would never see the direct result of the user input because update_cells would be called because the user input has a chance to be printed to the screen)
+        // the downside of doing it this way is that that a user keypress actually effects the state of the next cell update, and not the current cell update (the one that the user is currently looking at), although this is only noticable at low framerates
+        if !paused {
+            board.update_cells();
+            board_updated = true;
+        }
+
+        // handle key presses
         // this only handles one key per frame but key_input has a buffer so if more than one key is pressed in one frame duration then each key press will still get handled on subsequent frames 
         match key_input.next() {
             Some(input) => {
@@ -213,11 +240,35 @@ fn play_game<W: io::Write, R: io::Read>(board: &mut Board, key_input: &mut termi
                         board.init_randomly(); 
                         board_updated = true;
                     },
+                    termion::event::Key::Char('c') => { // clear board
+                        board.occupied_cells = HashSet::new();
+                        board_updated = true;
+                    }
                     termion::event::Key::Char('f') => { // move forward one frame
                         if paused {
                             board.update_cells();
                             board_updated = true;
                         }
+                    }
+                    termion::event::Key::Right => cursor_position.x += 1,
+                    termion::event::Key::Down => cursor_position.y += 1,
+                    termion::event::Key::Left => cursor_position.x -= 1,
+                    termion::event::Key::Up => cursor_position.y -= 1,
+                    termion::event::Key::Char('h') => { // hide cursor
+                        if cursor_visible {
+                            write!(stdout, "{}", termion::cursor::Hide).ok();
+                        } else {
+                            write!(stdout, "{}", termion::cursor::Show).ok();
+                        }
+                        cursor_visible = !cursor_visible;
+                    }
+                    termion::event::Key::Char('a') => { // create/kill a cell
+                        if board.occupied_cells.contains(&cursor_position) {
+                            board.occupied_cells.remove(&cursor_position);
+                        } else {
+                            board.occupied_cells.insert(cursor_position.clone());
+                        }
+                        board_updated = true;
                     }
                     _ => {}
                 }
@@ -225,18 +276,23 @@ fn play_game<W: io::Write, R: io::Read>(board: &mut Board, key_input: &mut termi
             None => {} // a key wasn't pressed
         }
 
-        if !paused {
-            board.update_cells();
-            board_updated = true;
-        }
-
+        // print board
         if board_updated {
             write!(stdout, "{}{}", termion::cursor::Goto(1, 2), board).ok();
-            stdout.flush().ok(); // ensure all writes are printed to the screen
         }
 
-        // sleep for duration of one frame
-        thread::sleep(time::Duration::from_millis(30));
+        // ensure cursor is at correct location
+        cursor_position.bound(
+            0, 0, 
+            board.width as i16 - 1, board.height as i16 - 1
+        );
+        write!(stdout, "{}", termion::cursor::Goto(
+            cursor_position.x as u16 + 2, 
+            cursor_position.y as u16 + 2
+        )).ok();
+
+        stdout.flush().ok(); // ensure all writes are printed to the screen
+        thread::sleep(time::Duration::from_millis(30)); // sleep for duration of one frame
     }
 }
 
@@ -253,14 +309,11 @@ fn main() {
     // create object to read keyboard inputs from (use async_stdin instead of io::stdin so that calls to key_input.next are nonblocking)
     let mut key_input = termion::async_stdin().keys();
 
-    // hide cursor
-    write!(stdout, "{}", termion::cursor::Hide).ok();
-
     print_static_text(&mut stdout, &board);
 
     play_game(&mut board, &mut key_input, &mut stdout);
 
-    // make cursor visable again
+    // make sure cursor is visable
     write!(stdout, "{}", termion::cursor::Show).ok();
     stdout.flush().ok();
 }
