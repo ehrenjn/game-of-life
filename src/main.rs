8f354a1f9ba9c410c3fbf620ac4c338c3fa2bbf1
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2021-06-02 21:56:07 -0400

    made proper TUI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 /*
+Add cursor movement and placing cells
+Add commandline args
+    framerate
+    board size
+Maybe 2 rectangles side by side should be used to make a single square pixel(▒▒)
 Dont hardcode all the numbers
 Might make sense to only draw the first n-2 rows and columns or something
     this way things would move off the screen and die more naturally
@@ -10,7 +15,16 @@ Could use curses or something to make the printing way nicer
 
 use std::{fmt, iter, thread, time};
 use std::collections::{HashSet, HashMap};
-use rand::{self, Rng};
+use rand::Rng;
+use termion::{
+    self, 
+    input::TermRead, // for Stdin::keys method
+    raw::IntoRawMode, // for Stdout::into_raw_mode method
+};
+use std::io::{
+    self,
+    Write, // for RawTerminal::write_fmt (RawTerminal's impl for Write trait) (called by write!)
+};
 
 
 // derive() will automatically derive all the traits needed to be hashable by autogenering an impl
@@ -41,6 +55,7 @@ impl Board {
     }
 
     fn init_randomly(&mut self) {
+        self.occupied_cells = HashSet::new(); // empty occupied_cells
         let mut rng = rand::thread_rng();
         for _ in 0..((self.width * self.height) / 4) {
             let random_cell = Point{
@@ -63,7 +78,7 @@ impl Board {
             let on_left_edge = cell.x == 0;
 
             // find all valid neighbours
-            let mut neighbours: Vec<Point> = Vec::with_capacity(0);
+            let mut neighbours: Vec<Point> = Vec::with_capacity(8);
             if !on_top_edge {
                 neighbours.push(Point{x: cell.x, y: cell.y - 1});
             }
@@ -120,17 +135,17 @@ impl fmt::Display for Board {
         // build empty board string
         let mut board_string = Vec::new();
         for _ in 0..self.height {
-            let mut cell_row: Vec<char> = iter::repeat(' ')
-                .take(self.width)
-                .collect(); // collect is a generic function but rust knows I want collect<Vec<char>> because thats the type of cell_row
+            let mut cell_row: Vec<char> = vec!['║'];
+            cell_row.extend(iter::repeat(' ').take(self.width));
+            cell_row.push('║');
+            cell_row.push('\r'); // in raw mode terminals a newline just moves the cursor down, we need a carriage return so that the cursor also moves to the beginning of the line
             cell_row.push('\n');
             board_string.push(cell_row);
         }
 
         // add filled cells
-        // !!! have to iterate over &self.occupied_cells instead of just self.occupied_cells because iterating over a raw vector consumes it (passes it by value into into_iter and thus moves it), borrowing it makes iteration no longer consume it (because you're just passing the borrowed value into the function so nothing is moved), could also do self.occupied_cells.iter() (since iter doesnt consume its caller) buts more characters
         for point in &self.occupied_cells {
-            board_string[point.y][point.x] = '#';
+            board_string[point.y][point.x+1] = '■'; // x+1 because the first character of every row is a '║'
         }
 
         // convert vector to string and print
@@ -140,13 +155,110 @@ impl fmt::Display for Board {
 }
 
 
+const INSTRUCTIONS: &str = "\
+    ║ spacebar: play/pause      ║\r\n\
+    ║ f:        forward 1 frame ║\r\n\
+    ║ r:        randomize       ║\r\n\
+    ║ q:        quit            ║\r\n\
+    ╚═══════════════════════════╝\
+";
+const INSTRUCTIONS_WIDTH: u16 = 29;
+const INSTRUCTIONS_HEIGHT: u16 = 5;
+
+
+// prints parts of screen that wont change
+#[allow(unused_must_use)] // so I dont have to type .ok() after every write! call
+fn print_static_text<W: Write>(stdout: &mut W, board: &Board) {
+
+    // hide cursor
+    write!(stdout, "{}", termion::cursor::Hide);
+
+    // print top and bottom of board
+    write!(stdout, "{}", termion::clear::All); // .ok() to convert Result into an Option and throw away the possible Error (because not handling the error is a compiler warning)
+    write!(stdout, "{}╔", termion::cursor::Goto(1, 1));
+    let long_pipe: String = iter::repeat('═').take(board.width).collect();
+    write!(stdout, "{}", long_pipe);
+    write!(stdout, "╗");
+    write!(
+        stdout, "{}╠", 
+        termion::cursor::Goto(1, board.height as u16 + 2)
+    );
+    write!(stdout, "{}", long_pipe);
+    write!(stdout, "╝");
+
+    // print instructions
+    write!(
+        stdout, "{}╦", 
+        termion::cursor::Goto(INSTRUCTIONS_WIDTH, board.height as u16 + 2)
+    );
+    write!(stdout, "\r\n{}", INSTRUCTIONS);
+
+    stdout.flush();
+}
+
+
 fn main() {
-    let mut board = Board::new(190, 44);
+    let mut board = Board::new(150, 30);
     board.init_randomly();
 
-    loop {
-        board.update_cells();
-        println!("\n\n\n===\n{}", board);
-        thread::sleep(time::Duration::from_millis(20)); // sleep for duration of one frame
+    // enter raw terminal mode
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+
+    // create object to read keyboard inputs from (use async_stdin instead of io::stdin so that calls to key_input.next are nonblocking)
+    let mut key_input = termion::async_stdin().keys();
+
+    print_static_text(&mut stdout, &board);
+
+    let mut paused = false;
+    let mut game_running = true;
+
+    while game_running {
+
+        let mut board_updated = false; // if set to true then we should draw the board even if the game is currently paused
+
+        // this only handles one key per frame but key_input has a buffer so if more than one key is pressed in one frame duration then each key press will still get handled on subsequent frames 
+        match key_input.next() {
+            Some(input) => {
+                match input.unwrap() {
+                    termion::event::Key::Char('q') => game_running = false,
+                    termion::event::Key::Char(' ') => paused = !paused,
+                    termion::event::Key::Char('r') => { // initialize randomly
+                        board.init_randomly(); 
+                        board_updated = true;
+                    },
+                    termion::event::Key::Char('f') => { // move forward one frame
+                        if paused {
+                            board.update_cells();
+                            board_updated = true;
+                        }
+                    }
+                    _ => {}
+                }
+            },
+            None => {} // a key wasn't pressed
+        }
+
+        if !paused {
+            board.update_cells();
+            board_updated = true;
+        }
+
+        if board_updated {
+            write!(stdout, "{}{}", termion::cursor::Goto(1, 2), board).ok();
+            stdout.flush().ok(); // ensure all writes are printed to the screen
+            thread::sleep(time::Duration::from_millis(30)); // sleep for duration of one frame
+        }
     }
+
+    // exit nicely
+    let screen_bottom = termion::cursor::Goto(
+        1,
+        board.height as u16 + INSTRUCTIONS_HEIGHT + 3
+    );
+    write!(
+        stdout, "{}{}", 
+        termion::cursor::Show, // make cursor visable again
+        screen_bottom
+    ).ok();
+    stdout.flush().ok();
 }
